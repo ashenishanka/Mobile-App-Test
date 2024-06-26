6e8f88ce972cfd0a2e9ed613f1fa49bedf1efaa2
Ashen Ishanka <ashen.ishanka74@gmail.com>
2024-06-26 13:06:38 +0530

    Login screen completed

--- Mobile-App-Test/ViewController.swift
+++ Mobile-App-Test/ViewController.swift
@@ -31,6 +31,8 @@ class ViewController: UIViewController {
         // Add tap gesture recognizer to the view
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
               view.addGestureRecognizer(tapGesture)
+        
+        userAuthenticated()
     }
     @objc func hideKeyboard() {
            view.endEditing(true)
@@ -54,18 +56,21 @@ class ViewController: UIViewController {
             print("email is \(user.profile?.email ?? "error")")
             Auth.auth().signIn(with: credential) { result, error in
 
+                self.userAuthenticated()
               // At this point, our user is signed in
                // print(result)
             }
                 
         }
     }
-    
+    func showAlertController(_ title: String, _ message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .default,handler: nil))
+        self.present(alert, animated: true)
+    }
     @IBAction func btnLoginClicked(_ sender: UIButton) {
         if userNameTextField.text?.isEmpty == true || passwordTextField.text?.isEmpty == true{
-            let alert = UIAlertController(title: "Cannot Login!", message: "No empty field allowed", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Okay", style: .default,handler: nil))
-            self.present(alert, animated: true)
+            showAlertController("Cannot Login!","No empty field allowed")
         }else{
             let email = userNameTextField.text!
             let password = passwordTextField.text!
@@ -74,17 +79,44 @@ class ViewController: UIViewController {
     }
     private func performLogin(_ email: String, _ password: String){
         let alertLogin = UIAlertController(title: "Login In...", message: "We are checking your account! Please wait", preferredStyle: .alert)
+       
         let activityIndicator = UIActivityIndicatorView(style: .medium)
-               activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-               activityIndicator.startAnimating()
-        activityIndicator.leadingAnchor.constraint(equalTo: alertLogin.view.leadingAnchor,constant: -20).isActive = true
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.startAnimating()
+        
         alertLogin.view.addSubview(activityIndicator)
+        activityIndicator.leadingAnchor.constraint(equalTo: alertLogin.view.leadingAnchor,  constant: 20).isActive = true
+        activityIndicator.topAnchor.constraint(equalTo: alertLogin.view.topAnchor, constant: 10).isActive = true
         self.present(alertLogin, animated: true)
         
         Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
           guard let strongSelf = self else { return }
-          print(authResult)
+//          print(authResult)
+            if(error != nil){
+                strongSelf.showAlertController("Cannot login", "Please check your email or password")
+            }else{
+               
+                strongSelf.userAuthenticated()
+            }
         }
     }
 }
 
+extension ViewController{
+    func userAuthenticated(){
+        if Auth.auth().currentUser != nil{
+            //user already authenticated
+            openHomeViewController()
+        }
+    }
+    func openHomeViewController(){
+       let homeViewController = HomeViewController(nibName: "HomeViewController", bundle: nil)
+       // self.present(homeViewController, animated: true)
+        if let navigationController = self.navigationController{
+            navigationController.setViewControllers([homeViewController], animated: true)
+        }else{
+            showAlertController("Error", "Functional error! Please contact the developer")
+        }
+        
+    }
+}
